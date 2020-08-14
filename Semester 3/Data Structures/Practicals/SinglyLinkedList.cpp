1067c4bd7a66e246a56f034265965a853e62d197
Anshul Verma <itsanshulverma@gmail.com>
2020-08-14 13:35:08 +0530

    Added comments in SinglyLinkedList.cpp

--- Semester 3/Data Structures/Practicals/SinglyLinkedList.cpp
+++ Semester 3/Data Structures/Practicals/SinglyLinkedList.cpp
@@ -1,28 +1,34 @@
-// By Anshul Verma
-// 19/78065
+/*
+	By Anshul Verma
+	19/78065
+*/
 
 #include <iostream>
 
 using namespace std;
 
+/******************************************
+	SinglyLinkedList Class
+*******************************************/
 class SinglyLinkedList
 {
 	struct node
 	{
 		int data;
-		struct node* next;
+		struct node *next;
 	};
 	struct node *head, *temp, *ptr, *tail;
-	public:
+
+  public:
 	SinglyLinkedList()
 	{
-		head=NULL;
+		head = NULL;
 	}
 	~SinglyLinkedList()
 	{
-		cout<<"\nDeallocating memory...\n";
+		cout << "\nDeallocating memory...\n";
 		ptr = head;
-		while(ptr != NULL)
+		while (ptr != NULL)
 		{
 			temp = ptr->next;
 			delete ptr;
@@ -41,6 +47,10 @@ class SinglyLinkedList
 	void delete_at_loc();
 	void display();
 };
+
+/******************************************
+	Create List Function
+*******************************************/
 void SinglyLinkedList::create()
 {
 	int n;
@@ -49,9 +59,9 @@ void SinglyLinkedList::create()
 	{
 		temp = new node;
 		temp->next = NULL;
-		cout<<"\nEnter the node data : ";
-		cin>>n;
-		if(head==NULL)
+		cout << "\nEnter the node data : ";
+		cin >> n;
+		if (head == NULL)
 		{
 			head = temp;
 			head->data = n;
@@ -64,141 +74,184 @@ void SinglyLinkedList::create()
 			tail->next = temp;
 			tail = temp;
 		}
-		cout<<"Want to enter more nodes ? (y/n) : ";
-		cin>>ch;
-	}while(ch=='y');
+		cout << "Want to enter more nodes ? (y/n) : ";
+		cin >> ch;
+	} while (ch == 'y');
 }
+
+/******************************************
+	Display List Function
+*******************************************/
 void SinglyLinkedList::display()
 {
-	cout<<"\n\t";
-	if(head==NULL)
-		cout<<"Linked List is empty.";
+	cout << "\n\t";
+	if (head == NULL)
+		cout << "Linked List is empty.";
 	ptr = head;
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
-		cout<<"-->"<<ptr->data;
+		cout << "-->" << ptr->data;
 		ptr = ptr->next;
 	}
-	cout<<endl;
+	cout << endl;
 }
+
+/******************************************
+	Insert at End Function
+*******************************************/
 void SinglyLinkedList::insert_at_end()
 {
 	temp = new node;
-	cout<<"\nEnter the node data you want to insert at end : ";
-	cin>>temp->data;
+	cout << "\nEnter the node data you want to insert at end : ";
+	cin >> temp->data;
 	temp->next = NULL;
 	tail->next = temp;
 	tail = temp;
-	cout<<"Inserted!"<<endl;
+	cout << "Inserted!" << endl;
 }
+
+/***************************************************
+	Insert at Beginning Function
+****************************************************/
 void SinglyLinkedList::insert_at_begin()
 {
 	temp = new node;
-	cout<<"\nEnter the node data you want to insert at beginning : ";
-	cin>>temp->data;
+	cout << "\nEnter the node data you want to insert at beginning : ";
+	cin >> temp->data;
 	temp->next = head;
 	head = temp;
-	cout<<"Inserted!"<<endl;
+	cout << "Inserted!" << endl;
 }
+
+/***************************************************
+	Insert at a Location Function
+****************************************************/
 void SinglyLinkedList::insert_at_loc()
 {
 	temp = new node;
 	int loc;
-	cout<<"\nEnter the location for new node : ";
-	cin>>loc;
-	cout<<"Enter the node data : ";
-	cin>>temp->data;
+	cout << "\nEnter the location for new node : ";
+	cin >> loc;
+	cout << "Enter the node data : ";
+	cin >> temp->data;
 	ptr = head;
-	for(int i=1; i<loc-1; i++)
+	for (int i = 1; i < loc - 1; i++)
 	{
 		ptr = ptr->next;
 	}
 	temp->next = ptr->next;
 	ptr->next = temp;
-	cout<<"Inserted!"<<endl;
+	cout << "Inserted!" << endl;
 }
+
+/*******************************************
+	Delete at End Function
+********************************************/
 void SinglyLinkedList::delete_at_end()
 {
 	ptr = head;
-	while(ptr->next->next != NULL)
+	while (ptr->next->next != NULL)
 	{
 		ptr = ptr->next;
 	}
 	delete ptr->next;
 	ptr->next = NULL;
 	tail = ptr;
-	cout<<"\nDeleted one node from end!"<<endl;
+	cout << "\nDeleted one node from end!" << endl;
 }
+
+/**************************************************
+	Delete at Beginning Function
+***************************************************/
 void SinglyLinkedList::delete_at_begin()
 {
 	temp = head;
 	head = head->next;
 	delete temp;
-	cout<<"\nDeleted one node from beginning!"<<endl;
+	cout << "\nDeleted one node from beginning!" << endl;
 }
+
+/***************************************************
+	Delete at a Location Function
+****************************************************/
 void SinglyLinkedList::delete_at_loc()
 {
 	int loc;
-	cout<<"\nEnter the location of node to delete : ";
-	cin>>loc;
+	cout << "\nEnter the location of node to delete : ";
+	cin >> loc;
 	ptr = head;
-	for(int i=1; i<loc-1; i++)
+	for (int i = 1; i < loc - 1; i++)
 	{
 		ptr = ptr->next;
 	}
 	temp = ptr->next;
 	ptr->next = ptr->next->next;
 	delete temp;
-	cout<<"\nDeleted from location : "<<loc<<"!"<<endl;
+	cout << "\nDeleted from location : " << loc << "!" << endl;
 }
+
+/******************************************
+	Count Function
+*******************************************/
 void SinglyLinkedList::count()
 {
-	int count=0;
-	ptr=head;
-	while(ptr != NULL)
+	int count = 0;
+	ptr = head;
+	while (ptr != NULL)
 	{
 		ptr = ptr->next;
 		++count;
 	}
-	cout<<"\nThere are "<<count<<" nodes in the list."<<endl;
+	cout << "\nThere are " << count << " nodes in the list." << endl;
 }
+
+/******************************************
+	Search Function
+*******************************************/
 void SinglyLinkedList::search()
 {
-	int n, count=0, flag=0;
-	cout<<"\nEnter the node data to be searched : ";
-	cin>>n;
+	int n, count = 0, flag = 0;
+	cout << "\nEnter the node data to be searched : ";
+	cin >> n;
 	ptr = head;
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		++count;
-		if(ptr->data == n)
+		if (ptr->data == n)
 		{
-			flag=1;
+			flag = 1;
 			break;
 		}
 		ptr = ptr->next;
 	}
-	if(flag=1)
-		cout<<"Data found at location "<<count<<" in the list."<<endl;
+	if (flag = 1)
+		cout << "Data found at location " << count << " in the list." << endl;
 	else
-		cout<<"Data not found in the list.";
-}
-void SinglyLinkedList::reverse()
-{
-	 ptr=head->next;
-	 head->next = NULL;
-	 tail = head;
-	 while(ptr!= NULL)
-	 {
-	 	temp = ptr;
-	 	ptr=ptr->next;
-	 	temp->next = head;
-		 head = temp;
-	 }
-	 cout<<"\nReversed!"<<endl;
-	 display();
+		cout << "Data not found in the list.";
 }
 
+/******************************************
+	Reverse Function
+*******************************************/
+void SinglyLinkedList::reverse()
+{
+	ptr = head->next;
+	head->next = NULL;
+	tail = head;
+	while (ptr != NULL)
+	{
+		temp = ptr;
+		ptr = ptr->next;
+		temp->next = head;
+		head = temp;
+	}
+	cout << "\nReversed!" << endl;
+	display();
+}
+
+/******************************************
+	Main Function
+*******************************************/
 int main()
 {
 	int choice;
@@ -206,48 +259,48 @@ int main()
 	SinglyLinkedList list;
 	do
 	{
-		cout<<"\n1. Create\n2. Insert at Beginning\n3. Insert at End\n4. Insert at Location\n5. Delete at Beginning\n6. Delete at End\n7. Delete at Location\n8. Count the nodes\n9. Search\n10. Reverse the list\n11. Display\n12. Exit"<<endl;
-		cout<<"Enter your choice : ";
-		cin>>choice;
-		switch(choice)
+		cout << "\n1. Create\n2. Insert at Beginning\n3. Insert at End\n4. Insert at Location\n5. Delete at Beginning\n6. Delete at End\n7. Delete at Location\n8. Count the nodes\n9. Search\n10. Reverse the list\n11. Display\n12. Exit" << endl;
+		cout << "Enter your choice : ";
+		cin >> choice;
+		switch (choice)
 		{
-			case 1:
-				list.create();
-				break;
-			case 2:
-				list.insert_at_begin();
-				break;
-			case 3:
-				list.insert_at_end();
-				break;
-			case 4:
-				list.insert_at_loc();
-				break;
-			case 5:
-				list.delete_at_begin();
-				break;
-			case 6:
-				list.delete_at_end();
-				break;
-			case 7:
-				list.delete_at_loc();
-				break;
-			case 8:
-				list.count();
-				break;
-			case 9:
-				list.search();
-				break;
-			case 10:
-				list.reverse();
-				break;
-			case 11:
-				list.display();
-				break;
-			case 12:
-				exit(0);
+		case 1:
+			list.create();
+			break;
+		case 2:
+			list.insert_at_begin();
+			break;
+		case 3:
+			list.insert_at_end();
+			break;
+		case 4:
+			list.insert_at_loc();
+			break;
+		case 5:
+			list.delete_at_begin();
+			break;
+		case 6:
+			list.delete_at_end();
+			break;
+		case 7:
+			list.delete_at_loc();
+			break;
+		case 8:
+			list.count();
+			break;
+		case 9:
+			list.search();
+			break;
+		case 10:
+			list.reverse();
+			break;
+		case 11:
+			list.display();
+			break;
+		case 12:
+			exit(0);
 		}
-		cout<<"\nWant to operate more? (y/n) ";
-		cin>>ch;
-	}while(ch=='y');
+		cout << "\nWant to operate more? (y/n) ";
+		cin >> ch;
+	} while (ch == 'y');
 }
